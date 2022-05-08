c5b9a00b65a083d9f3de3ae46e5cbc264aa45676
sumitsasidharan <s.sumit196@gmail.com>
2022-05-08 12:44:02 +0530

    Next Router , useRouter tuts

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import Link from 'next/link'
 
 export default function AboutPage() {
   return (
@@ -6,6 +6,8 @@ export default function AboutPage() {
        <h1>About</h1>
        <p>This is an app to find the lastest DJ and other musical events</p>
        <p>Version: 1.0.0</p>
+
+       
     </div>
   )
 }
--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,9 +1,13 @@
-import React from 'react'
+import { useRouter } from 'next/router';
 
 export default function EventPage() {
+   const router = useRouter();
+   console.log(router.query)
+
   return (
     <div>
        <h1>My Event</h1>
+       <h3>{router.query.slug}</h3>
     </div>
   )
 }
