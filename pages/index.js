9e52b64cd511a322e3e70d9a4d059de14b34f681
sumitsasidharan <s.sumit196@gmail.com>
2022-06-12 20:40:13 +0530

    modified home index

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function HomePage({events}) {
     <Layout>
        <h1>Upcoming Events</h1>
 
+      {/* can render a loader if events array is 0 */}
        {events.length === 0 && <h3>No events to show</h3>}
 
        {events.map(evt => (
