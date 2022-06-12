8e4bbfe8b018c77061e3c9a3adf47f7fad57d1ca
sumitsasidharan <s.sumit196@gmail.com>
2022-06-12 18:03:07 +0530

    updated eventitem and index home

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,34 @@
-import Link from "next/link"
-import Head from 'next/head';
 import Layout from "@/components/Layout";
+import EventItem from "@/components/EventItem";
 import { API_URL } from "@/config/index";
+import Link from "next/link";
 
 export default function HomePage({events}) {
   return (
     <Layout>
        <h1>Upcoming Events</h1>
+
+       {events.length === 0 && <h3>No events to show</h3>}
+
+       {events.map(evt => (
+         <EventItem key={evt.id} evt={evt} />
+       ))}
+
+       {events.length > 0 && (
+         <Link href='/events'>
+            <a className="btn-secondary">view all events</a>
+         </Link>
+       )}
    </Layout>
   )
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
    const res = await fetch(`${API_URL}/api/events`);
    const events = await res.json();
 
    return {
-      props: {events},
-      revalidate: 1
-   }
+      props: { events: events.slice(0, 3) },
+      revalidate: 1,
+   };
 }
