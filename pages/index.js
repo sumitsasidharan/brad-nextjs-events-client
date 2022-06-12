253ddf52e60a6e542fcbb66746e2d9c2af68c2e6
sumitsasidharan <s.sumit196@gmail.com>
2022-06-12 13:00:27 +0530

    api and fetch

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,22 @@
 import Link from "next/link"
 import Head from 'next/head';
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-export default function HomePage() {
+export default function HomePage({events}) {
   return (
     <Layout>
-       <h1>Home</h1>
-
-      
-    </Layout>
+       <h1>Upcoming Events</h1>
+   </Layout>
   )
+}
+
+export async function getServerSideProps() {
+   const res = await fetch(`${API_URL}/api/events`);
+   const events = await res.json();
+
+   return {
+      props: {events},
+      revalidate: 1
+   }
 }
