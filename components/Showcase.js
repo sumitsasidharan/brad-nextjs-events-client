cb0e5ddc11f4e0861707863d8a99c7163c469841
sumitsasidharan <s.sumit196@gmail.com>
2022-06-12 11:43:16 +0530

    api tuts

---
+++ components/Showcase.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from '@/styles/Showcase.module.css';
+
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h1>Welcome to the Party!</h1>
+      <h2>Find the hottest dj events</h2>
+    </div>
+  )
+}
